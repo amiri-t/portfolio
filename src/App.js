e5af57335fa9d2b6c5362b0b8077b5376ffb5570
Amir Tahiri <tahiriamir49@gmail.com>
2023-08-16 14:53:32 +0200

    File & Folder Set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "light" : "dark");
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <Navbar switchTheme={switchTheme} theme={theme} />
+      <Routes>
+        hello
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Navbar = () => {
+  return <Container>Navbar</Container>;
+};
+const Container = styled.div`
+  color: var(--fontPrimaryColor);
+`;
+
+export default Navbar;
